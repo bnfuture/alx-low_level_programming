f6f2ba58a96ec7e6fa1a02b1ba6c50640f8a20e0
bnfuture <bensonjohn534@gmail.com>
2023-05-09 10:04:48 +0000

    file i/o update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
 		to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 		do {
-			if (from == -1 || r == -1)
+		if (from == -1 || r == -1)
 	{
 		dprintf(STDERR_FILENO,
 		"Error: Can't read from file %s\n", argv[1]);
@@ -77,7 +77,9 @@ int main(int argc, char *argv[])
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_WRONLY | O_APPEND);
 
-		} while (r > 0);
+	}
+
+	while (r > 0);
 
 	free(buffer);
 	close_file(from);
